481655ecfde471de2b34579b3e7261266987b1b0
silvajr20 <silvanojr17@live.com>
2021-04-23 23:22:07 -0300

    commit inicial

---
+++ cadastro.veiculos/src/test/java/com/cad/veiculos/cadastro/veiculos/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cad.veiculos.cadastro.veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
